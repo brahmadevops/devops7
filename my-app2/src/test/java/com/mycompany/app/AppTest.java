7ea5567490672bbf76f68c34425fca61f9918371
root <root@ip-172-31-23-187.us-east-2.compute.internal>
2019-09-10 07:37:47 +0000

    test

--- my-app2/src/test/java/com/mycompany/app/AppTest.java
+++ my-app2/src/test/java/com/mycompany/app/AppTest.java
@@ -7,19 +7,6 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-<<<<<<< HEAD
-
-/**
-* editing and adding by @develop_user1
-hello 
-=======
-/** Hi this is modified by @develop_user2 
-Please merger this code into @develop
-
-end by @develop_user2
->>>>>>> develop_user2
-
-*/
 public class AppTest 
 {
     /**
@@ -29,6 +16,5 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
-	System.out.println("hello Develop_user1");
     }
 }
